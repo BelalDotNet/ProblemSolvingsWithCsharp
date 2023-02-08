e7e9e46138e4fde929c2fa1470e62b527abb6826
Belal Hossain <55940608+BelalDotNet@users.noreply.github.com>
2023-02-08 14:22:15 +0600

    Added BirthDay Candles Soliving

---
+++ Practice_Console/BirthDayCandles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Console
+{
+    internal class BirthDayCandles
+    {
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            var maxElement = candles.Max();
+            var maxCount = candles.Count(x => x == maxElement);
+            return maxCount;
+        }
+    }
+}
