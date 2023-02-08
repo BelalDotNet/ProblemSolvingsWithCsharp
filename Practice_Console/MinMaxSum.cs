252db0924e8ac6e4873923e7c04f5e79e7caa74e
Belal Hossain <55940608+BelalDotNet@users.noreply.github.com>
2023-02-08 11:52:20 +0600

    Min Max Sum Added

---
+++ Practice_Console/MinMaxSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Console
+{
+    internal class MinMaxSum
+    {
+        public static void miniMaxSum(List<int> arr)
+        {
+            long min = 0;
+            long max = 0;
+            arr.Sort();
+
+            for (int i = 0; i < arr.Count() - 1; i++)
+            {
+                min += arr[i];
+            }
+            for (int i = 1; i < arr.Count; i++)
+            {
+                max += arr[i];
+            }
+
+            Console.WriteLine(min + " " + max);
+        }
+
+    }
+
+}
