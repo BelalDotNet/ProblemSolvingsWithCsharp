022eb81e5d8b72fbf0a16c082125efb58811b0ef
Belal Hossain <55940608+BelalDotNet@users.noreply.github.com>
2023-02-12 11:14:54 +0600

    Time Conversion 12 To 24 With String input Added

---
+++ Practice_Console/TimeConversion12To24.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Console
+{
+    internal class TimeConversion12To24
+    {
+        public static string timeConversion(string s)
+        {
+            DateTime Parsed12Format = DateTime.Parse(s);
+            string Converted24Format = Parsed12Format.ToString("HH:mm: ss");
+            return Converted24Format;
+        }
+    }
+}
